71637488cdcbd91f60a8009ee61fbce66d2abe7e
Guilherme Silva dos Reis <32211071+yGuiOn@users.noreply.github.com>
2020-03-19 17:06:22 -0300

    Update mega_sena.py
    Adicionou a estrutura de repetição 'FOR'.
    Simplifiquei o código.

--- Exercícios/Projetos/Mega-Sena.py
+++ Exercícios/Projetos/Mega-Sena.py
@@ -1,12 +1,6 @@
 import random
-a = float(input('Quantos jogos quer ? '))
-n1 = random.randint(1,60)
-n2 = random.randint(1,60)
-n3 = random.randint(1,60)
-n4 = random.randint(1,60)
-n5 = random.randint(1,60)
-n6 = random.randint(1,60)
-jogo = n1,n2,n3,n4,n5,n6
-
-if a >= 1:
-    print('Os numeros são',random.randint(1,60),random.randint(1,60),random.randint(1,60),random.randint(1,60),random.randint(1,60),random.randint(1,60) )
+a = int(input('Quantos jogos quer ? '))
+for c in range(0, a):
+    for jogo in range(0, 6):
+        n = random.randint(1, 60)
+    print('Os numeros são', random.randint(1, 60), random.randint(1, 60), random.randint(1, 60), random.randint(1, 60), random.randint(1, 60), random.randint(1, 60))
