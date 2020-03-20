65fc4ff91dcca5bc1020f5c07e01f73c03903734
Guilherme Silva dos Reis <yGuiOn@github.com>
2020-03-20 19:41:04 -0300

    Melhoria do exercício!

--- Exercícios/Mundo 3/Ex83.py
+++ Exercícios/Mundo 3/Ex83.py
@@ -1,18 +1,18 @@
 #Exercício Python 083: Crie um programa onde o usuário digite uma expressão qualquer que use parênteses.
 #Seu aplicativo deverá analisar se a expressão passada está com os parênteses abertos e fechados na ordem correta.
 lista = []
-expresao = str(input('Digite a expressão: '))
+expressão = str(input('Digite uma expressão: '))
 
-for item in expresao:
-    if item in '(':
-        lista.append(item)
-    elif item in ')':
+for item in expressão:
+    if '(' in item:
+        lista.append('(')
+    if ')' in item:
         if '(' in lista:
             lista.remove('(')
         else:
-            lista.append(item)
-
+            lista.append(')')
+print(lista)
 if len(lista) == 0:
-    print('Sua expressão está valida')
+    print('Sua expressão está correta')
 else:
-    print('Sua expressão não está valida')
+    print('Sua expressão não está correta')
