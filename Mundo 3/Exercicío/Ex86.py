a15b3c349468c7172a1727d881200b6474061fa0
Guilherme Luan <trexguiluan@hotmail.com>
2020-04-03 19:56:29 -0300

    Update Ex86.py

--- Mundo 3/Exercicío/Ex86.py
+++ Mundo 3/Exercicío/Ex86.py
@@ -3,18 +3,14 @@ Exercício Python 086:
 Crie um programa que declare uma matriz de dimensão 3x3 e preencha com valores lidos pelo teclado.
 No final, mostre a matriz na tela, com a formatação correta.
 """
-matriz = [[], [], []]
+matriz = [[0, 0, 0],[0, 0, 0],[0, 0, 0]]
 
-for i in range(0, 3):
-    for j in range(0, 3):
-        n = int(input(f'Digite um valor para [{i},{j}]: '))
-        matriz[i].append(n)
-print()
-for e in range(0, 3):
-    if e > 0:
-        print('\n', end='') if e > 0 else ''
-    for c in range(0, 3):
-        print(f'[{matriz[e][c]:^6}]', end='')
-print()
+for linha in range(0,3):
+    for coluna in range(0,3):
+        matriz[linha][coluna] = int(input(f'Digite um valor para [{linha}, {coluna}]: '))
 
-
+for l in range(0,3):
+    for c in range(0,3):
+        print(f'[{matriz[l][c]:^6}]', end='')
+        
+    print()
