0e99b612903df8426c9cc46e8542b940e5a4b033
Guilherme Luan <trexguiluan@hotmail.com>
2020-04-10 14:34:54 -0300

    Update Ex95.py
    I fixed the alignment and I added the try and except.

--- Mundo 3/Exercicío/Ex95.py
+++ Mundo 3/Exercicío/Ex95.py
@@ -10,7 +10,7 @@ while True:
     dados['Nome'] = str(input('Nome do Jogador: '))
     partidas = int(input(f'Quantas partidas {dados["Nome"]} jogou? '))
     for p in range(0, partidas):
-        gols.append((int(input(f'   Quantos gols na partida {p} ? '))))
+        gols.append((int(input(f'   Quantos gols na partida {p+1} ? '))))
 
     while continuar not in 'SN':
         continuar = str(input('Quer continuar? [S/N] '))[0].upper().strip()
@@ -26,18 +26,20 @@ while True:
 print(25*'-=')
 
 print(f"{'cod':<4} {'Nome':<15} {'Gols':^15} {'Total':>10}")
+print(50*'-')
 for k, v in enumerate(time):
     print(f"{k:<4} {v['Nome']:<15}  {str(v['Gols']):^15} {v['Total']:^10}")
 
-print(25*'-=')
+print(50*'-')
 while True:
-    show = int(input('Mostrar dados de qual jogador? (999 para parar)'))
+    show = int(input('Mostrar dados de qual jogador? (999 para parar) '))
     if show == 999:
         break
     try:
         print(f'-- LEVANTAMENTO DO JOGADOR {time[show]["Nome"]}')
+        for k, v in enumerate(time[show]['Gols']):
+            print(f'    No jogo {k + 1} fez {v} gols')
     except:
-        print('ERRO! Esse jogador não foi registrado no banco de dados!')
+        print(f'ERRO! Não existe jogador com o número {show}')
 
-    for k, v in enumerate(time[show]['Gols']):
-        print(f'    No jogo {k + 1} fez {v} gols')
+print(f'{"<<< FIM DO PROGRAMA >>>":^50}')
