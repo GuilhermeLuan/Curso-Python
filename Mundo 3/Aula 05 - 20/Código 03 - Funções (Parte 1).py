de4cbd4bff4c8f4907709e91a462c1ae5f6199c1
Guilherme Luan <trexguiluan@hotmail.com>
2020-04-12 14:40:50 -0300

    Create Código 03 - Funções (Parte 1).py

---
+++ Mundo 3/Aula 05 - 20/Código 03 - Funções (Parte 1).py
@@ -0,0 +1,15 @@
+def soma(a, b): # a, b é o parâmetro
+    print(f'A = {a} e B = {b}')
+    s = a + b
+    print(f'A soma A + B = {s}')
+    print()
+
+
+# Programa Princiapl
+soma(4, 5) # subistitui o parâmetro a e b pelo 4 e 5
+soma(8, 9)
+soma(2, 1)
+
+#Outra forma mais explícita
+soma(a=4, b=5)
+soma(b=4 , a= 5)
