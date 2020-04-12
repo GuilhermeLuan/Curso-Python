7ef56b8ad5626ba9dbf0b03301a3dac94f4f91d7
Guilherme Luan <trexguiluan@hotmail.com>
2020-04-12 15:09:55 -0300

    Create Código 04 - Funções (Parte 1).py

---
+++ Mundo 3/Aula 05 - 20/Código 04 - Funções (Parte 1).py
@@ -0,0 +1,12 @@
+def soma(a, b): # a, b é o parâmetro
+    print(f'A = {a} e B = {b}')
+    s = a + b
+    print(f'A soma A + B = {s}')
+
+
+
+# Programa Princiapl
+soma(b=4, a=5) # subistitui o parâmetro a = 5, b = 4
+print(30*'-')
+soma(7, 2) # subistitui os parâmetros a e b pelo valor 7 e 2 respectivamente
+'soma(3, 9, 5)' # ERRO! pois só temos dois parâmentros 
