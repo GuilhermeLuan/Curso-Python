89d41a6cbc73f90aa0e40759530bc99215b2ec93
Guilherme Luan <trexguiluan@hotmail.com>
2020-04-13 17:15:48 -0300

    Create Ex98.py

---
+++ Mundo 3/Exercicío/Ex98.py
@@ -0,0 +1,25 @@
+'''
+Exercício Python 098: Faça um programa que tenha uma função chamada contador(), que receba três parâmetros: início, fim e passo. Seu programa tem que realizar três contagens através da função criada:
+a) de 1 até 10, de 1 em 1
+b) de 10 até 0, de 2 em 2
+c) uma contagem personalizada
+'''
+def contador(início, fim, meio):
+    print(30*'-')
+    print(f'Contagem de {início} ate {fim} de {meio} em {meio}')
+    meio = abs(meio) if meio != 0 else 1
+
+    if início < fim:
+        for n in range(início, fim + 1, meio):
+            print(n, end=' ')
+
+    if início > fim:
+        for n in range(início, fim + 1, -meio):
+            print(n, end=' ')
+    print('FIM')
+    print(30*'-')
+
+contador(1, 10, 1)
+contador(10, 0, -2)
+
+contador(int(input('Início: ')), int(input('Fim: ')), int(input('Passo: ')))
