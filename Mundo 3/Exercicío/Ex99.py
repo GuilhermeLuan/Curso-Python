cf3fc02719d57d2daa664e4eae635bbe5ca9cc83
Guilherme Luan <trexguiluan@hotmail.com>
2020-04-18 11:16:33 -0300

    Update Ex99.py
    I optimized the code

--- Mundo 3/Exercicío/Ex99.py
+++ Mundo 3/Exercicío/Ex99.py
@@ -1,24 +1,20 @@
 '''
 Exercício Python 099: Faça um programa que tenha uma função chamada maior(), que receba vários parâmetros com valores inteiros. Seu programa tem que analisar todos os valores e dizer qual deles é o maior.
 '''
-from random import randint
-
 def maior(*num):
-    temp = []
-    temp.append(num)
-
     print(20*'-=')
     print('Analisando os valores passados...')
 
-    for i, v in enumerate(temp[0]):
-        print(f'{v}',end=' ')
+    for n in num:
+        print(f'{n}',end=' ')
 
-    print(f'Foram informados {len(temp[0])} ao todo.')
+    print(f'Foram informados {len(num)} ao todo.')
 
-    if len(temp[0]) > 0:
-        print(f'O maior valor informado foi {max(temp[0])}')
+    if len(num) > 0:
+        print(f'O maior valor informado foi {max(num)}')
     else:
-        print('Não foi informado nenhum valor !')
+        m = 0
+        print(f'O maior valor informado foi {m}')
     print(20*'-=')
 
 maior(2, 9, 4, 5, 7, 1)
