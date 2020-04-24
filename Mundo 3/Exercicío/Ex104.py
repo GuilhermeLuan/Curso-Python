103b2b242f43397cd99c02a3c717c50572ff34fa
Guilherme Luan <trexguiluan@hotmail.com>
2020-04-24 19:48:39 -0300

    Update Ex104.py

--- Mundo 3/Exercicío/Ex104.py
+++ Mundo 3/Exercicío/Ex104.py
@@ -3,35 +3,16 @@ Exercício Python 104: Crie um programa que tenha a função leiaInt(), que vai 
 Ex: n = leiaInt('Digite um n: ')
 '''
 
-# def leiaInt(num=''):
-#     global n
-#     n = ''
-#     pergunta = input(num).strip()
-#     n = pergunta
-
-#     while n.isnumeric() == False:
-#         print('ERRO! Digite um número inteiro válido.')
-#         validação = input(num).strip()
-#         if validação.isnumeric() == True:
-#             return validação
-#     else:
-#         return n
-
-# n = leiaInt('Digite um número: ')
-# print(f'Você acabou de digitar o número: {n}')
-
-#Outra Solução
-
 def leiaInt(msg):
     ok = False
     valor = 0
     while True:
-        n = str(input(msg))
+        n = str(input(msg)).strip()
         if n.isnumeric():
             valor = int(n)
             ok = True
         else:
-            print('ERRO! Digite um número inteiro válido.')
+            print('ERRO! Digite um número inteiro válido')
         if ok:
             break
     return valor
