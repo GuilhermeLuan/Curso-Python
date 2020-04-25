dfab3678f8ff0019b7c1b4c2d406b72b2ec518d1
Guilherme Luan <trexguiluan@hotmail.com>
2020-04-25 15:05:39 -0300

    Update Ex072.py

--- Mundo 3/Exercicío/Ex072.py
+++ Mundo 3/Exercicío/Ex072.py
@@ -2,8 +2,8 @@
 # Crie um programa que tenha uma dupla totalmente preenchida com uma contagem por extenso, de zero até vinte.
 # Seu programa deverá ler um número pelo teclado (entre 0 e 20) e mostrá-lo por extenso.
 
-numero = ('zero', 'um', 'dois', 'três', 'quatro', 
-'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 
+numero = ('zero', 'um', 'dois', 'três', 'quatro',
+'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze',
 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete',
  'dezoito', 'dezenove', 'vinte')
 
