ae01bb808b2d1560939fd5cb0d540542df0a45bd
Guilherme Luan <trexguiluan@hotmail.com>
2020-04-25 12:55:11 -0300

    Update Ex106.py

--- Mundo 3/Exercicío/Ex106.py
+++ Mundo 3/Exercicío/Ex106.py
@@ -1,4 +1,6 @@
-#Exercício Python 106: Faça um mini-sistema que utilize o Interactive Help do Python. O usuário vai digitar o comando e o manual vai aparecer. Quando o usuário digitar a palavra 'FIM', o programa se encerrará. Importante: use cores.
+#Exercício Python 106
+#Faça um mini-sistema que utilize o Interactive Help do Python.
+#O usuário vai digitar o comando e o manual vai aparecer. Quando o usuário digitar a palavra 'FIM', o programa se encerrará. Importante: use cores.
 from time import sleep
 
 
