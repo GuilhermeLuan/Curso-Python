3dc305efa1e771115025d46ab3f3bb2c898e56a3
Guilherme Luan <trexguiluan@hotmail.com>
2020-04-26 19:21:36 -0300

    aulda 22

--- Mundo 3/Aula 07 - 22/Códigos/números.py
+++ Mundo 3/Aula 07 - 22/Códigos/números.py
@@ -1,10 +1,12 @@
-# Móludos e Pacotes
-# Modolarização - Ato de construir modulos
-#   Objetivo: Dividir um programa grande, aumantar a legibilidade, facilitar a manutenção
+"""
+Móludos e Pacotes
+Modolarização - Ato de construir modulos
+    Objetivo: Dividir um programa grande, aumantar a legibilidade, facilitar a manutenção"""
+
 import uteis
 
 num = int(input('Digite um valor '))
-fat = fatorial(num)
+fat = uteis.fatorial(num)
 print(f'O factorial de {num} é {fat}')
 print(f'O dobro de {num} é {uteis.dobro(num)}')
 print(f'O triplo de {num} é {uteis.triplo(num)}')
