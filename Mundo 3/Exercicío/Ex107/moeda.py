b2cfc44669b718aa2fe1e2a3da3ff643b4650a98
Guilherme Luan <trexguiluan@hotmail.com>
2020-04-26 20:03:19 -0300

    Exercicío #107

--- Mundo 3/Exercicío/Ex107/moeda.py
+++ Mundo 3/Exercicío/Ex107/moeda.py
@@ -1,7 +1,11 @@
 # Exercício Python 107
 # Crie um módulo chamado moeda.py que tenha as funções incorporadas aumentar(), diminuir(), dobro() e metade().
 # Faça também um programa que importe esse módulo e use algumas dessas funções.
-import moeda
 
-preco = float(input('Digite um preço: R$'))
-moeda.aumentar(preco)
+# def dobro(v):
+#     dobro
+
+
+def aumentar(v):
+    aumentar = v + (v * v/100)
+    print(f'Aumentando 10%, temos {aumentar}')
