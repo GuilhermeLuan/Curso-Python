e476b99fa6d2f271859f6ed309d9ba683bd3b3a2
Guilherme Luan <trexguiluan@hotmail.com>
2020-04-26 18:46:15 -0300

    Create Código 01 - Módulos e Pacotes.py

---
+++ Mundo 3/Aula 07 - 22/Código 01 - Módulos e Pacotes.py
@@ -0,0 +1,13 @@
+'''
+Móludos e Pacotes
+'''
+def fatorial(n):
+    f = 1
+        f *= c
+    return f
+
+
+
+num = int(input('Digite um valor '))
+fat = fatorial(num)
+print(f'O factorial de {num} é {fat}')
