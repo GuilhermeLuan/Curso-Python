e98197e4292acc36c38132454de5390e6ed69a4d
Guilherme Luan <trexguiluan@hotmail.com>
2020-04-26 19:53:12 -0300

    Ex107

---
+++ Mundo 3/Exercicío/Ex107/principal.py
@@ -0,0 +1,3 @@
+#Exercício Python 107
+#Crie um módulo chamado moeda.py que tenha as funções incorporadas aumentar(), diminuir(), dobro() e metade().
+#Faça também um programa que importe esse módulo e use algumas dessas funções.
