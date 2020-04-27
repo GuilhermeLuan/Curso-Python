4da675019e3dca636bc7c28d5ce5521cf5edc7b5
Guilherme Luan <trexguiluan@hotmail.com>
2020-04-26 21:21:19 -0300

    Update moeda_2.py

--- Mundo 3/Exercicío/Ex108/moeda_2.py
+++ Mundo 3/Exercicío/Ex108/moeda_2.py
@@ -2,27 +2,25 @@
 # Adapte o código do desafio #107, criando uma função adicional chamada moeda() que consiga mostrar os números como um valor monetário formatado.
 
 
-def dobro(v):
+def dobro(v=0):
     res = v * 2
     return res
 
 
-def metade(v):
+def metade(v=0):
     res = v / 2
     return res
 
 
-def aumentar(v, taxa):
+def aumentar(v=0, taxa=0):
     res = v + (v * taxa/100)
     return res
 
 
-def diminuir(v, taxa):
+def diminuir(v=0, taxa=0):
     res = v - (v * taxa/100)
     return res
 
 
-def formatacao(v):
-    valor = str(v).replace('.', ',')
-    res = f'R${valor}'
-    return res
+def formatacao(v=0, moeda='R$'):
+    return f'{moeda}{v:.2f}'.replace('.', ',')
