e6b4d62780c6f56585281f6f3376862cd4878060
Guilherme Luan <trexguiluan@hotmail.com>
2020-04-26 21:22:22 -0300

    Exercício #109

---
+++ Mundo 3/Exercicío/Ex109/principal.py
@@ -0,0 +1,9 @@
+# Exercício Python 108
+# Adapte o código do desafio #107, criando uma função adicional chamada moeda() que consiga mostrar os números como um valor monetário formatado.
+
+import moeda_2 as moeda
+
+price = float(input('Digite um preço: R$'))
+print(f'A metade de {moeda.formatacao(price)} é {moeda.formatacao(moeda.metade(price))}')
+print(f'O dobro de {moeda.formatacao(price)} é {moeda.formatacao(moeda.dobro(price))}')
+print(f'Aumentando 10%, temos {moeda.formatacao(moeda.aumentar(price, 10))}')
