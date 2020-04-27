df535a267ddc308db7a4ecfc55479fa966b16e91
Guilherme Luan <trexguiluan@hotmail.com>
2020-04-26 22:37:32 -0300

    Update principal.py

--- Mundo 3/Exercicío/Ex108/principal.py
+++ Mundo 3/Exercicío/Ex108/principal.py
@@ -1,5 +1,6 @@
-# Exercício Python 108
-# Adapte o código do desafio #107, criando uma função adicional chamada moeda() que consiga mostrar os números como um valor monetário formatado.
+# Exercício Python 109:
+# Modifique as funções que form criadas no desafio 107 para que elas aceitem um parâmetro a mais, informando se o valor retornado por elas vai ser ou não formatado pela função moeda(), desenvolvida no desafio 108.
+
 
 import moeda_2 as moeda
 
