44a5b070ff3dfc5ecfdf8d4e5d1320dfc4d2fdb4
Guilherme Luan <trexguiluan@hotmail.com>
2020-04-27 18:59:35 -0300

    Exercício #109

--- Mundo 3/Exercicío/Ex109/moeda_3.py
+++ Mundo 3/Exercicío/Ex109/moeda_3.py
@@ -1,24 +1,34 @@
-# Exercício Python 108
-# Adapte o código do desafio #107, criando uma função adicional chamada moeda() que consiga mostrar os números como um valor monetário formatado.
+# Exercício Python 109:
+# Modifique as funções que form criadas no desafio 107 para que elas aceitem um parâmetro a mais,
+# informando se o valor retornado por elas vai ser ou não formatado pela função moeda(), desenvolvida no desafio 108.
 
 
-def dobro(v=0):
+
+def dobro(v=0, form=False):
     res = v * 2
+    if form:
+        return formatacao(res)
     return res
 
 
-def metade(v=0):
+def metade(v=0, form=False):
     res = v / 2
+    if form:
+        return formatacao(res)
     return res
 
 
-def aumentar(v=0, taxa=0):
+def aumentar(v=0, taxa=0, form=False):
     res = v + (v * taxa/100)
+    if form:
+        return formatacao(res)
     return res
 
 
-def diminuir(v=0, taxa=0):
+def diminuir(v=0, taxa=0, form=False):
     res = v - (v * taxa/100)
+    if form:
+        return formatacao(res)
     return res
 
 
--- Mundo 3/Exercicío/Ex109/principal.py
+++ Mundo 3/Exercicío/Ex109/principal.py
@@ -1,9 +1,10 @@
-# Exercício Python 108
-# Adapte o código do desafio #107, criando uma função adicional chamada moeda() que consiga mostrar os números como um valor monetário formatado.
+# Exercício Python 109:
+# Modifique as funções que form criadas no desafio 107 para que elas aceitem um parâmetro a mais,
+# informando se o valor retornado por elas vai ser ou não formatado pela função moeda(), desenvolvida no desafio 108.
 
-import moeda_2 as moeda
+import moeda_3 as moeda
 
 price = float(input('Digite um preço: R$'))
-print(f'A metade de {moeda.formatacao(price)} é {moeda.formatacao(moeda.metade(price))}')
-print(f'O dobro de {moeda.formatacao(price)} é {moeda.formatacao(moeda.dobro(price))}')
-print(f'Aumentando 10%, temos {moeda.formatacao(moeda.aumentar(price, 10))}')
+print(f'A metade de {moeda.formatacao(price)} é {moeda.metade(price, True)}')
+print(f'O dobro de {moeda.formatacao(price)} é {moeda.dobro(price, True)}')
+print(f'Aumentando 10%, temos {moeda.aumentar(price, 10, True)}')
