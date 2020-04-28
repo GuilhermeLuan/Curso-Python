5eb369a916475ece89192f5e2668dbced478c704
Guilherme Luan <trexguiluan@hotmail.com>
2020-04-27 21:39:05 -0300

    Exercício #111

--- Mundo 3/Exercicío/Ex111/principal.py
+++ Mundo 3/Exercicío/Ex111/principal.py
@@ -1,7 +1,7 @@
 # Exercício Python 110
 # Adicione o módulo moeda.py criado nos desafios anteriores,
 # uma função chamada resumo(), que mostre na tela algumas informações geradas pelas funções que já temos no módulo criado até aqui.
-import utilidadescev as moeda
+from utilidadescev import moeda
 
 price = float(input('Digite um preço: R$'))
 moeda.resumo(price, 20, 12)
