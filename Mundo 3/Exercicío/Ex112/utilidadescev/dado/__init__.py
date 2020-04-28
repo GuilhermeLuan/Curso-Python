10a056d4a49a229e4606e03871723e22f499a5b7
Guilherme Luan <trexguiluan@hotmail.com>
2020-04-28 18:09:50 -0300

    Update __init__.py

--- Mundo 3/Exercicío/Ex112/utilidadescev/dado/__init__.py
+++ Mundo 3/Exercicío/Ex112/utilidadescev/dado/__init__.py
@@ -1,3 +1,9 @@
+# Exercício Python 112
+# Dentro do pacote utilidadesCeV que criamos no desafio 111,
+# temos um módulo chamado dado. Crie uma função chamada leiaDinheiro()
+# que seja capaz de funcionar como a função imputa(),
+# mas com uma validação de dados para aceitar apenas valores que seja
+
 def leiaDinheiro(valor):
     while True:
         n = str(input(valor)).strip()
