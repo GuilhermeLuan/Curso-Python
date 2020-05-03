67509a3e719af9b0c7924586fbb9c050fed45b66
Guilherme Luan <trexguiluan@hotmail.com>
2020-05-03 16:14:33 -0300

    Create Ex113.py

---
+++ Mundo 3/Exercicío/Ex113.py
@@ -0,0 +1,40 @@
+"""
+Exercício Python 113:
+Reescreva a função leiaInt() que fizemos no desafio 104, incluindo agora a possibilidade da digitação de um número de tipo inválido.
+Aproveite e crie também uma função leiaFloat() com a mesma funcionalidade.
+"""
+
+
+def leiaInt(msg):
+    ok = False
+    valor = 0
+    while True:
+        n = str(input(msg)).strip()
+        try:
+            valor = int(n)
+            ok = True
+        except ValueError:
+            print('ERRO! Digite um número inteiro válido')
+        if ok:
+            break
+    return valor
+
+
+def leiaFloat(msg):
+    ok = False
+    valor = 0
+    while True:
+        n = str(input(msg)).strip()
+        try:
+            valor = float(n)
+            ok = True
+        except ValueError:
+            print('ERRO! Digite um número real válido')
+        if ok:
+            break
+    return valor
+
+
+inteiro = leiaInt('Digite um número inteiro: ')
+real = leiaFloat('Digite um número real: ')
+print(f'O valor inteiro digitado foi {inteiro} e o real foi {real}')
