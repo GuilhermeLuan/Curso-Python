bce1157870dd135ab7bd9112ac29dcfd6d0ba759
Guilherme Luan <trexguiluan@hotmail.com>
2020-05-07 20:54:45 -0300

    Exercício 115

--- Mundo 3/Exercicío/Ex115/main.py
+++ Mundo 3/Exercicío/Ex115/main.py
@@ -1,4 +1,4 @@
 # Exercício Python 115a: Vamos criar um menu em Python, usando modularização.
-from lib import interface as programa
+from comandos import interface as programa
 
 programa.start_interface()
