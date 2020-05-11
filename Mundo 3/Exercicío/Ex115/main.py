11a622f77189338f60c2607b49183c3953468571
Guilherme Luan <trexguiluan@hotmail.com>
2020-05-11 19:08:39 -0300

    Update main.py

--- Mundo 3/Exercicío/Ex115/main.py
+++ Mundo 3/Exercicío/Ex115/main.py
@@ -1,22 +1,23 @@
 # Exercício Python 115a: Vamos criar um menu em Python, usando modularização.
 from time import sleep
-from comandos import interface as p
+
 from comandos import arquivo as a
+from comandos import interface as i
 
 a.criar_arquivo()
 while True:
 
-    res = p.menu(['Ver pessoas cadastradas', 'Cadastrar nova pessoa', 'Sair do sistema'])
+    res = i.menu(['Ver pessoas cadastradas', 'Cadastrar nova pessoa', 'Sair do sistema'])
     if res == 1:
-        p.text('PESSOAS CADASTRADAS')
+        i.text('PESSOAS CADASTRADAS')
         a.pessoas()
         sleep(0.5)
     elif res == 2:
-        p.text('Opção 2')
+        i.text('Opção 2')
         sleep(0.5)
     elif res == 3:
         sleep(0.5)
-        p.text('Saindo do sistema... Até logo!')
+        i.text('Saindo do sistema... Até logo!')
         break
     else:
         print('\033[31mERRO! Digite uma opção valida\033[m')
